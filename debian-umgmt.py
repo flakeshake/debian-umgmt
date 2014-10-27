1d7b3be5b7b950df97267ffae2d0efc79453380f
D. Thekumthala <d.thekumthala@googlemail.com>
2014-10-27 13:55:48 +0100

    Restructured code into several functions

--- debian-umgmt.py
+++ debian-umgmt.py
@@ -3,46 +3,54 @@
 # Copyright (c) 2014 by Dennis Thekumthala , see the file "LICENSE" for details
 
 import csv
+import subprocess32
 from whiptail import Whiptail
 
 
-pwhead =  ["Username" , "Password"  , "User ID" , "Group ID" , "Name", "Home Directory" , "Login Shell"]
+pwhead =  ["Username" , "Password"  , "User ID" , "Group ID" , "User Info", "Home Directory" , "Login Shell"]
 grhead =  ["Group Name" , "Password"  , "Group ID" , "Members"]
-passwd = open("/etc/passwd")
-groups = open("/etc/group")
+
 csv.register_dialect('unixpwd', delimiter=':', quoting=csv.QUOTE_NONE)
-pwreader = csv.DictReader(passwd, fieldnames = pwhead , dialect = 'unixpwd')
-grreader = csv.DictReader(groups, fieldnames = grhead , dialect = 'unixpwd')
 
 
-ui = Whiptail("User managment" , height=25, width=78)
-userdb = {}
-groupdb = {}
-whmenu = []
-for row in pwreader:
-	name = row["Name"].split(',')[0]
-	row["Name"] = name
-        tupl = (row["Username"] , name)
-        whmenu.append(tupl)
+def userlist(userdb ,ui):
+    whmenu = [("Add new user ..", "")]
+    list =  [(userdb[entry]["Username"], userdb[entry]["Full Name"]) for entry in userdb.keys()]
+    whmenu.extend(list)
+    ret = ui.menu("Choose an user" , whmenu ,' ')	
+    return ret
+
+
+def loadusr():
+    userdb = {}
+    passwd = open("/etc/passwd")
+    pwreader = csv.DictReader(passwd, fieldnames = pwhead , dialect = 'unixpwd') 
+    for row in pwreader:
+        name = row["User Info"].split(',')[0]
+	row["Full Name"] = name
         row["Group memberships"] = []
         # a dictionary of dictionaries
 	userdb[row["Username"]] = row
+    passwd.close()
+    return userdb
 
 
-for row in grreader:
+
+def loadgrp():
+    groupdb = {}
+    groups = open("/etc/group")
+    grreader = csv.DictReader(groups, fieldnames = grhead , dialect = 'unixpwd') 
+    for row in grreader:
         grname = row["Group Name"]
         members = row["Members"].split(",")
         # cross reference users and groups
         for member in members:
             if len(member) != 0:
               userdb[member]["Group memberships"].append(grname)
-        # a dictionary of dictionaries
-	groupdb[row["Group Name"]] = row
+	groupdb[row["Group Name"]] = row   
+    groups.close()
+    return groupdb
 
-
-
-passwd.close()
-groups.close()
 
 
 def modgrp (username):
@@ -52,14 +60,47 @@ def modgrp (username):
     val = ui.checklist("Select or deselect groups user " + username + " belongs to." , list)
     print val
     return 
-    
+
+def newuser(ui):
+   uname = ui.prompt("Please enter an username:")
+   fname = ui.prompt("Please enter the full name of this user:")
+   return    
+
+def rmuser(user):
+    return    
+
+def chpw(user):
+    return    
+
+ui = Whiptail("User managment" , height=25, width=78)
+
 while True:
-   # ui.backtitle  = "Choose an user."
-   ret = ui.menu("Choose an user" , whmenu)
-   chuser = userdb[ret]
-   chuser["Password"] = "(hidden)"
-   chuser["Group memberships"] =  ' '.join(chuser["Group memberships"])  
-   
-   ret = ui.menu("Select the setting you want to modify.", chuser.items(), ' ')
-   if ret == "Group memberships":
-      modgrp(chuser["Username"])
+   ui.title  = "User managment"
+   userdb = loadusr()
+   groupdb = loadgrp()
+   ret = userlist(userdb , ui)
+
+   if ret == "Add new user ..":
+     ui.title = "Add new user"  
+     newuser(ui)
+   else:
+     chuser = userdb[ret]
+     filter =  ["Username" , "Full Name"  , "User ID" , "Group ID" , "Home Directory" , "Login Shell" , "Password Settings" , "Group memberships" , "Delete this user"]
+     selected = []
+     for key in filter:
+	if key in chuser:
+         value = chuser[key]
+         if key == "Group memberships":
+          selected.append((key,' '.join(value)))
+         else:
+          selected.append((key,value))
+        else:
+          selected.append((key,' '))  
+     ret = ui.menu("Select the setting you want to modify.", selected, ' ')
+     if ret == "Group memberships":
+        modgrp(chuser["Username"])
+     if ret  == "Password settings":
+        chpw(chuser["Username"])
+     if ret == "Delete this user":
+        rmuser(chuser["Username"])
+
